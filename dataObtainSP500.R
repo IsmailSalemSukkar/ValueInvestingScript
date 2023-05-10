a4805705a97181c62a9dcd3cd176eca9c7cc681e
Ismail Sukkar <ismailsukkar@gmail.com>
2023-05-09 23:13:57 -0400

    finito

--- dataObtainSP500.R
+++ dataObtainSP500.R
@@ -4,6 +4,7 @@ library(yfinance)
 
 ###Data Obtaining###
 #######Pull Data from yahoo##########
+
 stocks <- tq_index("SP500")
 
 dataOGG <- getQuote(unique(stocks$symbol),
@@ -14,11 +15,42 @@ dataOGG <- getQuote(unique(stocks$symbol),
 
 dataOG <- rownames_to_column(dataOGG, "Symbol")
 
-dataO <- get_bs(stocks$symbol, report_type = "quarterly")
-
+dataO <- get_bs(stocks$symbol)
 
 dataOO <- dataO %>% group_by((ticker)) %>% slice_max(date)
 
+
+
 data <- inner_join(dataOG, dataOO, by = c("Symbol" = "ticker"))
 
-write.csv(data, "stock500_raw.csv")
+
+
+######Beta Calculation####
+Market <- as.data.frame(getSymbols("^GSPC", auto.assign=FALSE))
+Market <- cbind(Date = rownames(Market), Market)
+
+betaCalc <- data.frame(matrix(ncol=2,nrow=0))
+colnames(betaCalc) <- c("Symbol","Beta")
+
+for (i in stocks$symbol) {
+  Stock <- as.data.frame(getSymbols(i, auto.assign=FALSE))
+  Stock <- cbind(Date = rownames(Stock), Stock)
+  joinedStocks <- inner_join(Stock, Market, by = c("Date"))
+  marketReturn <- diff(log(joinedStocks$GSPC.Adjusted))
+  stockReturn <- diff(log(joinedStocks[,paste0(i,".Adjusted")]))
+  fit <- lm(stockReturn~marketReturn)
+  beta = fit$coefficients[2]
+  append_row <- cbind(i,beta)
+  colnames(append_row) <- c("Symbol","Beta")
+  betaCalc <- rbind(betaCalc,append_row)
+}
+
+
+data <- inner_join(data, betaCalc, by= c("Symbol"))
+
+
+write.csv(data, "Data/stock500_raw.csv")
+setwd("../")
+
+
+getwd()
